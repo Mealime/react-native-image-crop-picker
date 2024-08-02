24a0a8bf69793f3880a6ca91fe20290b07160a01
Iman <iheid00@safeway.com>
2024-08-01 20:57:28 -0700

    Update PickerModuleImpl.java free crop style

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModuleImpl.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModuleImpl.java
@@ -113,7 +113,7 @@ class PickerModuleImpl {
         options.setCompressionFormat(Bitmap.CompressFormat.JPEG);
         options.setCompressionQuality(100);
         options.setCircleDimmedLayer(false);
-        options.setFreeStyleCropEnabled(false);
+        options.setFreeStyleCropEnabled(true);
         options.setShowCropGrid(true);
         options.setShowCropFrame(true);
         options.setHideBottomControls(true);
@@ -126,7 +126,6 @@ class PickerModuleImpl {
                 .withOptions(options);
 
             if (config.width > 0 && config.height > 0) {
-                uCrop.withAspectRatio(config.width, config.height);
                 uCrop.withMaxResultSize(config.width, config.height);
             }
 
