4ecb2513ed07c6e410bc64dcd6df2f91381e00fd
jcro128 <Jeffrey.Crowe@albertsons.com>
2026-02-18 12:45:17 -0800

    rn 0.77

--- ios/src/ImageCropPicker.m
+++ ios/src/ImageCropPicker.m
@@ -887,9 +887,7 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
             CGFloat heightRatio = [[self.options objectForKey:@"height"] floatValue];
             if (widthRatio > 0 && heightRatio > 0){
                 CGSize aspectRatio = CGSizeMake(widthRatio, heightRatio);
-                // TODO: This line breaks as part of custom cake app build, disabling it for now.
-                // cropVC.customAspectRatio = aspectRatio;
-                [cropVC setValue:[NSValue valueWithCGSize:aspectRatio] forKey:@"customAspectRatio"];              
+                cropVC.aspectRatioPreset = aspectRatio;              
             }
             cropVC.aspectRatioLockEnabled = ![[self.options objectForKey:@"freeStyleCropEnabled"] boolValue];
             cropVC.resetAspectRatioEnabled = !cropVC.aspectRatioLockEnabled;
