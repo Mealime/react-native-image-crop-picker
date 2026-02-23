59fadc06681c3130874785983d958fa05c0d3b40
jcro128 <Jeffrey.Crowe@albertsons.com>
2026-02-23 11:34:05 -0800

    pin older version of lib

--- ios/src/ImageCropPicker.m
+++ ios/src/ImageCropPicker.m
@@ -887,7 +887,8 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
             CGFloat heightRatio = [[self.options objectForKey:@"height"] floatValue];
             if (widthRatio > 0 && heightRatio > 0){
                 CGSize aspectRatio = CGSizeMake(widthRatio, heightRatio);
-                cropVC.aspectRatioPreset = aspectRatio;              
+                // cropVC.aspectRatioPreset = aspectRatio;
+                [cropVC setValue:[NSValue valueWithCGSize:aspectRatio] forKey:@"customAspectRatio"];  
             }
             cropVC.aspectRatioLockEnabled = ![[self.options objectForKey:@"freeStyleCropEnabled"] boolValue];
             cropVC.resetAspectRatioEnabled = !cropVC.aspectRatioLockEnabled;
